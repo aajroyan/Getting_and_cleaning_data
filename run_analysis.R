91897270710c7677986e5f9a1b1d16b33a531a6c
aajroyan <aajroyan@hotmail.com>
2014-09-21 16:50:06 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,6 +3,7 @@ TESTING = read.table("test/X_test.txt")
 DataSet = rbind(TRAINING,TESTING)
 vars = read.table("features.txt")
 vars = vars[,2]
+names(DataSet) = vars
 MeanNstd = DataSet[,grepl( "mean" , names( DataSet ) ) | grepl( "std" , names( DataSet ) )]
 AvgData = sapply(MeanNstd, mean)
 write.table(AvgData, file = "course_project.txt", row.name=FALSE)
